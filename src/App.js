536c5cf7f7b86a52287cbbd10d92f975f3ec9229
Paul <euclyd@gmail.com>
2022-09-20 22:25:11 +0100

    timeframe function

--- src/App.js
+++ src/App.js
@@ -1,63 +1,73 @@
 import './App.css';
 import jsonData from './data.json';
 import React, {useState} from 'react';
-import Card from './Card';
+import Card, {CardHead} from './Card';
 import './images/icon-exercise.svg';
 import { v4 as uuidv4 } from 'uuid';
 //uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
 
 
+
 function App() {
 
   const [getTime, setTime] = useState(jsonData);
-  
-  var orderInfo = getTime.map( function(order) {
-
+  let color;
   
   
 
-    
-    let color;
+  
+  const orderInfo = getTime.map(({title, timeframes})=> {
+
+    var hours;
+    function changePeriod(timeframe){
+       hours = timeframes.daily.current
+      console.log(hours);
+       //spread the json 
+        //setTime(hours);
+        return hours;
+    }
+
     let key=uuidv4();
-
-    
+  
+    switch(title){
+    case 'Work':
+    color='orange';
+    break;
+    case 'Play':
+    color='soft-blue';
+    break;
+    case 'Study':
+    color='light-red';
+    break;
+    case 'Exercise':
+    color='lime-green';
+    break;
+    case 'Social':
+    color='violet';
+    break;
+    case 'Self Care':
+    color='soft-orange';
+  }
   
  
-    switch(order.title){
-      case 'Work':
-      color='orange';
-      break;
-      case 'Play':
-      color='soft-blue';
-      break;
-      case 'Study':
-      color='light-red';
-      break;
-      case 'Exercise':
-      color='lime-green';
-      break;
-      case 'Social':
-      color='violet';
-      break;
-      case 'Self Care':
-      color='soft-orange';
-    }
-    
-    return (
-      
-    <Card key={key} title={order.title} time={order.timeframes.daily.current} bgColor={color}/>);
-  });
+
+ return (   
+  <Card key={key} title={title} time={hours} bgColor={color} changePeriod={changePeriod}/>
+  )}
+  )
 
 
 
-  var header_card=<React.Fragment><button onClick={()=>console.log('daily')}>daily</button></React.Fragment>;
+
+
+  // var header_card=<React.Fragment><button onClick={()=>console.log('daily')}>daily</button></React.Fragment>;
   
   
   return ( 
     
     <div className='container'>
-      <Card title='item-1' bgColor='blue' header_card={header_card} />
-      {orderInfo}
+       <CardHead key='0' title='item-1' bgColor='blue' time={['daily', 'weekly', 'monthly']} header_card={true}  />
+     {orderInfo}
       </div>
     
     
--- src/Card.js
+++ src/Card.js
@@ -3,22 +3,59 @@ import './App.css'
 
 const Card = (props) => {
 
-
-
- // console.log(props.time);
-//console.log(text);
   return (
     <div className={`color ${props.title} ${props.bgColor}`} >
-        <div className={`card ${props.title}`}>
-          {props.title}
-          {props.header_card}
-          <React.Fragment><button onClick={()=>console.log('daily')}>daily</button></React.Fragment>
+        <div className={`card ${props.title} dark-blue`}>
+          {props.title} 
+          {props.hours} 
+          {<React.Fragment>
+            <ul>
+              <li>
+                <button onClick={()=>props.changePeriod('daily')}>daily</button>
+                </li>
+                <li>
+                  <button onClick={()=>props.changePeriod('weekly')}>weekly</button>
+                  </li>
+                  <li>
+                    <button onClick={()=>props.changePeriod('monthly')}>monthly</button>
+                    </li>
+                    </ul>
+                    </React.Fragment>}
         </div>
+        
         </div>
 
     
   )
 }
 
+const CardHead = (props) => {
+
+  return (
+    
+        <div className={`card ${props.title} dark-blue`}>
+          <div className={`color ${props.title} ${props.bgColor}`} >   
+            </div>
+          {<React.Fragment>
+            <ul>
+              <li>
+                <button onClick={()=>props.changePeriod('daily')}>{props.time[0]}</button>
+                </li>
+                <li>
+                  <button onClick={()=>props.changePeriod('weekly')}>{props.time[1]}</button>
+                  </li>
+                  <li>
+                    <button onClick={()=>props.changePeriod('monthly')}>{props.time[2]}</button>
+                    </li>
+                    </ul>
+                    </React.Fragment>}
+            
+         </div>
+
+    
+  )
+}
+
 export default Card
+export {CardHead}
 
