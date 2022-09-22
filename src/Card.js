7807ce448e9b0e010cee3f29b9c998e69ff73b11
Paul <euclyd@gmail.com>
2022-09-22 15:26:35 +0100

    working functionality!

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,41 @@
-import React from 'react'
+import React from 'react';
 import './App.css'
+import jsonData from './data.json';
 
-const Card = (props) => {
+const Card = ({id, data, time}) => {
+
+  let color = 'White';
+    switch(data.title){
+    case 'Work':
+    color='orange';
+    break;
+    case 'Play':
+    color='soft-blue';
+    break;
+    case 'Study':
+    color='light-red';
+    break;
+    case 'Exercise':
+    color='lime-green';
+    break;
+    case 'Social':
+    color='violet';
+    break;
+    case 'Self Care':
+    color='soft-orange';
+    break;
+    default:
+    break;
+  }
+  
 
   return (
-    <div className={`color ${props.title} ${props.bgColor}`} >
-        <div className={`card ${props.id} dark-blue`}>
-        <div className="title">{props.title}</div>
-          <div className="hours-large">{props.current} Hours</div>
-          <div className="previous">{props.previous} last time</div>
+    <div className={`color ${id} ${color}`} >
+        <div className={`card ${id} dark-blue`}>
+        <div className="title">{data.title}</div>  
+          <div className="hours-large">{data.timeframes[`${time}`].current} Hours</div>
+          <div className="previous">{data.timeframes[`${time}`].previous} last time</div>
+      
         </div>
         
         </div>
@@ -17,23 +44,23 @@ const Card = (props) => {
   )
 }
 
-const CardHead = (props) => {
-
+const CardHead = ({title, bgColor, period, changePeriod}) => {
+ 
   return (
     
-        <div className={`card ${props.title} dark-blue`}>
-          <div className={`color ${props.title} ${props.bgColor}`} >   
+        <div className={`card ${title} dark-blue`}>
+          <div className={`color ${title} ${bgColor}`} >   
             </div>
           
             <ul>
               <li>
-                <button onClick={()=>props.changePeriod('daily')}>{props.time[0]}</button>
+                <button onClick={()=>changePeriod('daily')}>{period[0]}</button>
                 </li>
                 <li>
-                  <button onClick={()=>props.changePeriod('weekly')}>{props.time[1]}</button>
+                  <button onClick={()=>changePeriod('weekly')}>{period[1]}</button>
                   </li>
                   <li>
-                    <button onClick={()=>props.changePeriod('monthly')}>{props.time[2]}</button>
+                    <button onClick={()=>changePeriod('monthly')}>{period[2]}</button>
                     </li>
                     </ul>
                     
